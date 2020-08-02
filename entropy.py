6584fb63969559bb0d18f6d94fc491495fd03280
wenboi <1796369212@qq.com>
2020-08-02 10:33:07 +0800

    熵权法计算

--- entropy.py
+++ entropy.py
@@ -1,28 +1,37 @@
 import numpy as np
 import xlrd
  
-#读数据并求熵
-path=u'/Users/blue/Downloads/indicators2.xls'
-hn,nc=1,1
-#hn为表头行数,nc为表头列数
-sheetname=u'table_indicators'
-def readexcel(hn,nc):
+
+
+#  从excel文件读取数据矩阵(去掉表头)
+def readexcel():
+    # 读数据并求熵
+    path = 'data/indicators2.xls'
+    sheetname = 'table_indicators'
     data = xlrd.open_workbook(path)
     table = data.sheet_by_name(sheetname)
     nrows = table.nrows
     data=[]
     for i in range(nrows):
-        data.append(table.row_values(i)[0:])
+        data.append(table.row_values(i))
     return np.array(data)
-def entropy(data0):
-    #返回每个样本的指数
-    #样本数，指标个数
-    n,m=np.shape(data0)
-    #一行一个样本，一列一个指标
-    #下面是归一化
-    maxium=np.max(data0,axis=0)
-    minium=np.min(data0,axis=0)
-    data= (data0-minium)*1.0/(maxium-minium)
+
+
+#  完成数据的归一化
+def normal(data):
+
+    # data = data.astype(np.float)
+    # 每一列的最值
+    maxium=np.max(data,axis=0)
+    minium=np.min(data,axis=0)
+    data= (data-minium)*1.0/(maxium-minium)
+    return data
+
+
+def entropy(data):
+
+    # 样本数，指标个数
+    n, m = np.shape(data)
     ##计算第j项指标，第i个样本占该指标的比重
     sumzb=np.sum(data,axis=0)
     data=data/sumzb
@@ -35,5 +44,11 @@ def entropy(data0):
     w=(1-e)/np.sum(1-e)
     recodes=np.sum(data*w,axis=1)
     return recodes
-data=readexcel(hn,nc)
-grades=entropy(data)
+
+
+if __name__ == '__main__':
+
+    data = readexcel()
+    data = normal(data)
+    weight = entropy(data)
+    print(weight)
